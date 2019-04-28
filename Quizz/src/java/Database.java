94e2f16f16bbbce28eb212a5e8da70c7811a1234
Stefano Hayba <hayba@outlook.com.br>
2019-04-28 19:49:00 -0300

    complemento do pacote question e database

--- Quizz/src/java/Database.java
+++ Quizz/src/java/Database.java
@@ -16,9 +16,21 @@ public class Database {
     private static ArrayList<User> users;
     private static ArrayList<Question> questions;
 
-    /**
-     * @param user
-     */
+    public static ArrayList<Question> getQuizMatematica(){
+      ArrayList<Question> quiz = new ArrayList<>();
+      quiz.add(new Question("Qual a capital de São Paulo?", "3", new String[]{"São Pedro","São Bastos","São Paulo"}));
+      quiz.add(new Question("Complete a frase: 'O rato roeu...'", "1", new String[]{"'A roupa do rei de Roma.'","'e morreu.'","'mas sobrevive com a ajuda de aparelhos.'"}));
+      quiz.add(new Question("Qual a capital da Bahia?", "2", new String[]{"Salvador","Belém","Salvadeiro"}));
+      quiz.add(new Question("Quantos ossos o corpo humano adulto possui?", "3", new String[]{"328","404","206"}));
+      quiz.add(new Question("Quantos dedos um humano normal possui em cada mão?", "3", new String[]{"3","4","5"}));
+      quiz.add(new Question("Qual dessas cores é primária?", "3", new String[]{"Roxo","Dourada","Azul"}));
+      quiz.add(new Question("Qual animal anda de quatro patas?", "3", new String[]{"Humano","Pato","Ornitorrínco"}));
+      quiz.add(new Question("Se você fosse um paladino, o que faria?", "3", new String[]{"Caçar","Nadar","Recuperar a Terra Santa"}));
+      quiz.add(new Question("Qual animal é ovíparo?", "3", new String[]{"Elefante","Cachorro","Ornitorrinco"}));
+      quiz.add(new Question("Qual animal é um mamífero?", "3", new String[]{"Cobra","Lagarto","Ornitorrínco"}));
+      return quiz;
+    }
+    
     public static void insertUser(User user) {
         users.add(user);
     }
--- Quizz/src/java/Question.java
+++ Quizz/src/java/Question.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,39 @@
  * @author Christian
  */
 public class Question {
+    private String pergunta;
+    private String resposta;
+    private String alternativas[];
+
+    public Question(String pergunta, String resposta, String[] alternativas) {
+        this.pergunta = pergunta;
+        this.resposta = resposta;
+        this.alternativas = alternativas;
+    }
+    
+    public String getPergunta() {
+        return pergunta;
+    }
+
+    public void setPergunta(String pergunta) {
+        this.pergunta = pergunta;
+    }
+
+    public String getResposta() {
+        return resposta;
+    }
+
+    public void setResposta(String resposta) {
+        this.resposta = resposta;
+    }
+
+    public String[] getAlternativas() {
+        return alternativas;
+    }
+
+    public void setAlternativas(String[] alternativas) {
+        this.alternativas = alternativas;
+    }
+
     
 }
